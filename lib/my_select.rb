7eb3d9d1c5ed45b8ad4bb3319f5d626328543470
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-05 15:53:33 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,8 +4,8 @@ def my_select(collection)
   while i< collection.length
   if yield(collection[i])== true
     new_collection<< yield(collection[i])
+  end
   i+=1 
- end 
  end
  new_collection
 end
